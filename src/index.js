29b2a56eeffd9bb9545b560e3d5f50447706b598
takutou <takuto1488@gmail.com>
2024-04-22 22:04:47 +0900

    scssに変更

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./index.css";
+import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { CookiesProvider } from "react-cookie";
@@ -13,7 +13,7 @@ root.render(
     <CookiesProvider>
       <App />
     </CookiesProvider>
-  </Provider>,
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
