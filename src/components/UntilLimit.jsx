4bcc74cf352ddacbc82f0edd0c04c9dc75092803
takutou <takuto1488@gmail.com>
2024-04-27 15:40:00 +0900

    期限を過ぎた場合のメッセージ変更

--- src/components/UntilLimit.jsx
+++ src/components/UntilLimit.jsx
@@ -7,23 +7,29 @@ export const UntilLimit = ({ limit }) => {
     const limitDate = new Date(limit);
     // 期限時刻がUTC時間になっているため日本時刻へ変換(-9時間)
     limitDate.setHours(limitDate.getHours() - 9);
+
     // s,m,h時間での合計
     const totalSecDateValue = parseInt((limitDate - nowDate) / 1000); // 秒
     const totalMinDate = totalSecDateValue / 60; // 分
     const totalHourDate = totalMinDate / 60;
     const totalDayDate = totalHourDate / 24;
-    // 表示する時間(余り)
-    const showMinDate = totalMinDate % 60; // 残り:分
-    const showHourDate = totalHourDate % 24; // 残り:時
-    // 小数点以下切り捨て|分の表示のみ、0分になってしまうため+1分
-    return (
-      parseInt(totalDayDate) +
-      "日と" +
-      parseInt(showHourDate) +
-      "時間" +
-      (parseInt(showMinDate) + 1) +
-      "分"
-    );
+    // 期限前か後か
+    if (totalSecDateValue > 0) {
+      // 表示する時間(余り)
+      const showMinDate = totalMinDate % 60; // 残り:分
+      const showHourDate = totalHourDate % 24; // 残り:時
+      // 小数点以下切り捨て|分の表示のみ、0分になってしまうため+1分
+      return (
+        parseInt(totalDayDate) +
+        "日と" +
+        parseInt(showHourDate) +
+        "時間" +
+        (parseInt(showMinDate) + 1) +
+        "分"
+      );
+    } else {
+      return "終了";
+    }
   }
 
   return (
